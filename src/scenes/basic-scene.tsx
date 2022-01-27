922abc56b55952397eab294cf4016a4fa4fe7010
hilary <zhang.hilary28@gmail.com>
2022-01-27 22:51:04 +0800

    transform objects

--- src/scenes/basic-scene.tsx
+++ src/scenes/basic-scene.tsx
@@ -13,15 +13,72 @@ const BasicScene = () => {
         // Scene
         const scene = new THREE.Scene()
 
-        // Red cube
-        const geometry = new THREE.BoxGeometry(1, 1, 1)
-        const material = new THREE.MeshBasicMaterial({
-            color: 0xff0000
-        })
+        /**
+         * Objects
+         */
+        const group = new THREE.Group()
+        group.position.y = 1
+        group.scale.y = 2
+        group.rotation.y = 1
+        scene.add(group)
 
-        const mesh = new THREE.Mesh(geometry, material)
+        const cube1 = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        )
+        group.add(cube1)
 
-        scene.add(mesh)
+        const cube2 = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+        cube2.position.x = -2
+        group.add(cube2)
+
+        const cube3 = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ color: 0x0000ff })
+        )
+        cube3.position.x = 2
+        group.add(cube3)
+
+        // // Red cube
+        // const geometry = new THREE.BoxGeometry(1, 1, 1)
+        // const material = new THREE.MeshBasicMaterial({
+        //     color: 0xff0000
+        // })
+        //
+        // const mesh = new THREE.Mesh(geometry, material)
+        // // mesh.position.x = 0.7
+        // // mesh.position.y = - 0.6
+        // // mesh.position.z = 2
+        // mesh.position.set(0.7, -0.6, 1)
+        //
+        // // scene.add(mesh)
+        //
+        // /**
+        //  * Scale
+        //  */
+        // // mesh.scale.x = 2
+        // // mesh.scale.y = 0.5
+        // // mesh.scale.z = 0.5
+        // mesh.scale.set(2, 0.5, 0.5)
+        //
+        // /**
+        //  * Rotation
+        //  */
+        // mesh.rotation.reorder('YXZ')
+        // mesh.rotation.x = Math.PI * 0.25
+        // mesh.rotation.y = Math.PI * 0.25
+
+            // Axes helper
+        const axesHelper = new THREE.AxesHelper(3)
+        scene.add(axesHelper)
+
+        // console.log(mesh.position.length())
+        // mesh.position.normalize()
+        // console.log(mesh.position.length())
+
 
         // Sizes
         const sizes = {
@@ -31,11 +88,13 @@ const BasicScene = () => {
 
         // Camera
         const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-        camera.position.z = 3
-        // camera.position.x = 2
+        // camera.position.x = 1
         // camera.position.y = 1
+        camera.position.z = 3
 
         scene.add(camera)
+
+        // camera.lookAt(mesh.position)
 
         // Renderer
         // Canvas
