64cc358647e20172c8acda847a2c8cc53dd8f5ef
zhanghaoran <zhanghaoran@hl.com>
2022-02-10 14:03:36 +0800

    lighst the third day.

--- src/scenes/lights.tsx
+++ src/scenes/lights.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
 import { OrbitControls  } from 'three/examples/jsm/controls/OrbitControls'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
 
 const Lights = () => {
     
@@ -18,66 +19,168 @@ const Lights = () => {
          // Scene
         const scene = new THREE.Scene()
 
+         /**
+         * Debug ui
+         */
+          const gui = new dat.GUI({
+            width: 400,
+            closed: true
+        })
+
         /**
          * Lights
          * 
-         * The AmbientLight applies omnidirectional lighting
-         * • color
-         * • intensity
-         */
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-        ambientLight.color = new THREE.Color(0xffffff)
-        ambientLight.intensity = 0.5
-        scene.add(ambientLight)
-
-        /**
-         * The directionalLight will have a sun-like effect as if the sun
-         * rays were traveling in paralle
-         * • intensity
-         * • color
-         */
-        const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
-        // To change the direction, move the light
-        directionalLight.position.set(1, 0.25, 0)
-        scene.add(directionalLight)
-
-        /**
-         * The HemishphereLight is similar to the AmbientLight but with
-         * a different color from the sky than the color coming from ground
-         * • color(or skyColor)
-         * • groundColor
-         * • intensity
-         */
-        const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.5)
-        scene.add(hemisphereLight)
-
-        /**
-         * The pointLight is almost like a lighter
-         * The light starts at an infinitely small point spreads uniformly in
-         * directions.
-         * • color
-         * • intensity
+         * Performance
+         * Lights can cost a lot when it comes to performances
+         * Try to add as few lights as possible and try to use the lights that cost less
+         * Minimal cost: AmbientLight/HemisphereLight
+         * Moderate cost: DirectionalLight/PointLight
+         * High cost: SpotLight/RectAreaLight
          * 
-         * By default, the light intensity doesn't fade.
-         * We can control the fade distance and how fast it fades with
-         * distance and decay
+         * Bake
+         * The idea is to bake the light into the texture
+         * This can be done in a 3D software
+         * The drawback is that we cannot move the light anymore and
+         * we have to load huge texture
+         * Example: https://threejs-journey.com/ baked.jpg
+         * 
+         * Helper
+         * To assist us with positioning the lights, we can use helpers
+         * HemisphereLightHelper
+         * DirectionalLightHelper
+         * PointLightHelper
+         * RectAreaLightHelper
+         * SpotLightHelper
          */
-        const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
-        pointLight.position.set(1, -0.5, 1)
-        scene.add(pointLight)
 
-        /** 
-         * The RectAreaLight works like the big ractangle lights
-         * you can see on the photoshoot set
-         * It's a mix between a directional light and a diffuse light
-         * • color 
-         * • intensity
-         * • width
-         * • height
-         */
-        const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
-        scene.add(rectAreaLight)
+        {
+            /**
+             * The AmbientLight applies omnidirectional lighting
+             * • color
+             * • intensity
+             */
+            const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+            ambientLight.color = new THREE.Color(0xffffff)
+            ambientLight.intensity = 0.5
+            scene.add(ambientLight)
+            gui.add(ambientLight, 'intensity', 0, 1, 0.001)
+        }
+        
+        {
+            /**
+             * The directionalLight will have a sun-like effect as if the sun
+             * rays were traveling in paralle
+             * • intensity
+             * • color
+             */
+            const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+            // To change the direction, move the light
+            directionalLight.position.set(1, 0.25, 0)
+            scene.add(directionalLight)
 
+            // Helper
+            const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+            scene.add(directionalLightHelper)
+        }
+       
+
+        {
+             /**
+             * The HemishphereLight is similar to the AmbientLight but with
+             * a different color from the sky than the color coming from ground
+             * • color(or skyColor)
+             * • groundColor
+             * • intensity
+             */
+            const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.5)
+            scene.add(hemisphereLight)
+
+            // Helper
+            const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+            scene.add(hemisphereLightHelper)
+        }
+       
+
+        {
+            /**
+             * The pointLight is almost like a lighter
+             * The light starts at an infinitely small point spreads uniformly in
+             * directions.
+             * • color
+             * • intensity
+             * 
+             * By default, the light intensity doesn't fade.
+             * We can control the fade distance and how fast it fades with
+             * distance and decay
+             */
+            const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
+            pointLight.position.set(1, -0.5, 1)
+            scene.add(pointLight)
+
+            // Helper
+            const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+            scene.add(pointLightHelper)
+        }
+        
+        {
+            /** 
+             * The RectAreaLight works like the big ractangle lights
+             * you can see on the photoshoot set
+             * It's a mix between a directional light and a diffuse light
+             * • color 
+             * • intensity
+             * • width
+             * • height
+             * 
+             * The RectAreaLight only works with MeshStandardMaterial and
+             * MeshPhysicalMaterial
+             */
+            const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 1)
+
+            // You can then move the light and rotate it
+            // You can also use lookAt(...) to rotate more easily
+            rectAreaLight.position.set(-1.5, 0, 1.5)
+            rectAreaLight.lookAt(new THREE.Vector3())
+            scene.add(rectAreaLight)
+
+            // RectAreaLightHelper isn't part of the three variable and
+            // we must import it
+            // we need to update it on the next frame and also update its
+            // position and rotation manually
+            //
+            // rectAreaLightHelper.position.copy(rectAreaLight.position)
+            // rectAreaLightHelper.quaternion.copy(rectAreaLight.quaternion)
+            // rectAreaLightHelper.update()
+            const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+            scene.add(rectAreaLightHelper)
+        }
+       
+        {
+             /**
+             * The SpotLight is like a flashlight
+             * It's a cone of light starting at a point and oriented in a direction
+             * • color
+             * • intensity
+             * • distance
+             * • angle
+             * • penumbra
+             * • decay
+             */
+            const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 6, Math.PI * 0.1, 0.5, 1)
+            spotLight.position.set(0, 2, 3)
+            scene.add(spotLight)
+            // To rotate the SpotLight, we need to add its target property to the scene
+            // and move it
+            spotLight.target.position.x = -0.75
+            scene.add(spotLight.target)
+
+            // Helper
+            // The SpotLight has no size, we also need to call its update(...) method on
+            // next frame after moving the target
+            const spotLigheHelper = new THREE.SpotLightHelper(spotLight)
+            scene.add(spotLigheHelper)
+        }
+       
 
         /**
          * Objects
@@ -160,18 +263,7 @@ const Lights = () => {
 
             // Call animate again on the next frame
             window.requestAnimationFrame(animate)
-        }
-
-       /**
-         * Debug ui
-         */
-        const gui = new dat.GUI({
-            width: 400,
-            closed: true
-        })
-
-        gui.add(ambientLight, 'intensity', 0, 1, 0.001)
-        
+        } 
 
         animate()
     }
